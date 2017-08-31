4c6f8d973a9922b8633dbcf55835c7a08ac9fcde
ericmpower <31522039+ericmpower@users.noreply.github.com>
2017-08-31 14:44:15 -0700

    Create main.py
    Created the main file for testing the .eplog filetype system.

---
+++ main.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python2.7
+#
+# Author: Eric Power
+# Date: August 31, 2017
+
+"""Main file for testing the .eplog filetype classes"""
+import sys
+
+def main():
+    """Main entry point for the script."""
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
